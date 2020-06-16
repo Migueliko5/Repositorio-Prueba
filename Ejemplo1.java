256ef7286fde838d6852792ccdfe983be97e8813
Migueliko5 <migueliko51999@gmail.com>
2020-06-16 19:44:02 +0200

    Subida de 2 archivos java

---
+++ Ejemplo1.java
@@ -0,0 +1,10 @@
+package tema5_practica2;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Este programa es de ejemplo para ser subido a GitHub");
+
+	}
+
+}
---
+++ Ejemplo2.java
@@ -0,0 +1,10 @@
+package tema5_practica2;
+
+public class Ejemplo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Si se esperaba algo aquí, lo siento por la decepción, lo importante aquí es saber usar Git y GitHub");
+
+	}
+
+}
